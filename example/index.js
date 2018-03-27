0e7580006ed2d7f104af1bed4b98dfd49435fd06
George Karagkiaouris <karaggeorge0@gmail.com>
2018-03-26 22:04:15 -0400

    Update example

--- example/index.js
+++ example/index.js
@@ -1,18 +1,13 @@
 const {h, render} = require('ink');
 const {Select, Option, Separator} = require('../lib');
-const readline = require('readline');
-
-// readline.emitKeypressEvents(process.stdin);
-// process.stdin.setRawMode(true);
-// process.stdin.resume();
 
 const unmount = render(
-  <Select onSelect={item => console.log(item)}>
+  <Select onSelect={item => console.log(item + ' was selected')}>
     <Option value='1'>One</Option>
-    <Option value='2' onSelect={() => console.log('HI')}>Two</Option>
+    <Option value='2' onSelect={() => console.log('Second Action')}>Two</Option>
     <Separator />
     <Option value='3'>Three</Option>
-    <Separator label='==============' />
+    <Separator label='======' />
     <Option value='4'>Four</Option>
   </Select>
 );
